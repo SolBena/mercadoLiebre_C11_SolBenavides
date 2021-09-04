e3234dc8761df6e8cf2608f742ed6dd0479f99ee
SolBena <sol.benavides95@gmail.com>
2021-09-03 22:40:43 -0300

    Agrego formularios

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html',)));
+app.get('/register.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 
-app.listen(3030, () => console.log("Servidor corriendo"));
+app.listen(3030, () => console.log("Servidor corriendo en puerto: " + port));
